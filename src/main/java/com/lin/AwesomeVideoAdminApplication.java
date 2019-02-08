2f9f644df8908394cd4dbfd0f3716cfe7e125a05
lkmc2 <lkmc2@163.com>
2019-02-08 16:56:16 +0800

    生成实体类，并配置Thymeleaf的支持

--- src/main/java/com/lin/AwesomeVideoAdminApplication.java
+++ src/main/java/com/lin/AwesomeVideoAdminApplication.java
@@ -2,8 +2,10 @@ package com.lin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.lin.dao")
 public class AwesomeVideoAdminApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/lin/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.lin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author lkmc2
+ * @date 2019/2/8
+ * @description 首页控制器
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("showInfo")
+    public String showInfo(Model model) {
+        model.addAttribute("name", "jack");
+        return "index";
+    }
+
+}
