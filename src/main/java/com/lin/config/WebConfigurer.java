a7d239d87c13f23ccd7f19424cf9d70cf9f3482a
lkmc2 <lkmc2@163.com>
2019-02-10 19:41:17 +0800

    修复登陆拦截器把静态资源也拦截的问题

--- src/main/java/com/lin/config/WebConfigurer.java
+++ src/main/java/com/lin/config/WebConfigurer.java
@@ -4,7 +4,6 @@ import com.lin.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -19,17 +18,14 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
 
-    // 这个方法是用来配置静态资源的，比如html，js，css，等等
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    }
-
     // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // addPathPatterns("/**") 表示拦截所有的请求，
         // excludePathPatterns("/user/login") 表示除了登陆之外，因为登陆不需要登陆也可以访问
-        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/user/login");
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login", "/static/**");
     }
 
 }
